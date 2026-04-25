0974e0fc3284f1435c254f1a0fb7aec3a1b134ad
wenyulin-11 <wenyu.lin@sjsu.edu>
2026-04-25 00:50:21 -0700

    1
    1

--- tutorials/node-drawing-app/server.js
+++ tutorials/node-drawing-app/server.js
@@ -1,26 +1,23 @@
-console.log('server is running');
-
 var express = require('express');
-
 var app = express();
+var server = app.listen(3000);
 
 app.use(express.static('public'));
 
+console.log("My socket server is running");
+
 var socket = require('socket.io');
-
-var server = app.listen(3000);
-
 var io = socket(server);
 
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
-  console.log('new connection ' + socket.id);
-  socket.on('mouse', mouseMsg);
-}
-function mouseMsg(data) {
+  console.log('new connection: ' + socket.id);
 
+  socket.on('mouse', mouseMsg);
+
+  function mouseMsg(data) {
     socket.broadcast.emit('mouse', data);
-    //io.sockets.emit('mouse', data);
     console.log(data);
-}
+  }
+} 
