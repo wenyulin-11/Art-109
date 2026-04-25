91f188d16dc775901bab76b277dba524d72519b0
wenyulin-11 <wenyu.lin@sjsu.edu>
2026-04-25 00:51:41 -0700

    1
    1

--- tutorials/node-drawing-app/server.js
+++ tutorials/node-drawing-app/server.js
@@ -5,7 +5,7 @@ var server = app.listen(3000);
 app.use(express.static('public'));
 
 console.log("My socket server is running");
-
+///
 var socket = require('socket.io');
 var io = socket(server);
 
